118d47cba80fdcbbb1f71b9afd114b2f14a305c2
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2025-02-07 09:58:23 -0500

    Update Hello.java
    Said hi to Bob

--- src/helloWorld2/Hello.java
+++ src/helloWorld2/Hello.java
@@ -6,5 +6,7 @@ public class Hello {
 		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World!");
 		System.out.println("I bid thee farewell.");
+		
+		System.out.println("Hi, Bob!");
 	}
 }
