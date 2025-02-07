37596a7441423e62d4607811475c18225902fd80
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2025-02-07 09:53:24 -0500

    Update Hello.java
    Added goodbye

--- src/helloWorld2/Hello.java
+++ src/helloWorld2/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World!");
+		System.out.println("I bid thee farewell.");
 	}
 }
