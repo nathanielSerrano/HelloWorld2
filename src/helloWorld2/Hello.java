06618ea7faae17d0615e6e8108179449d4ba1ea7
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2025-02-07 09:52:51 -0500

    Update Hello.java
    Added preamble message

--- src/helloWorld2/Hello.java
+++ src/helloWorld2/Hello.java
@@ -3,6 +3,7 @@ package helloWorld2;
 public class Hello {
 	
 	public static void main(String[] args) {
+		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World!");
 	}
 }
